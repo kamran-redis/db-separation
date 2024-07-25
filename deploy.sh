7b5b43aee3ab06445f3a055a2e919c6d849326de
root <root@kamran-bastion2.us-east1-b.c.central-beach-194106.internal>
2024-07-25 22:01:01 +0000

    simple version

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+kubectl create namespace  operator
+kubectl create namespace  application
+
+kubectl apply -f bundle.yaml  -n operator
+kubectl apply -f role.yaml -n application
+kubectl apply -f role_binding.yaml -n application
+kubectl apply -f svc-rec-application.yaml  -n operator
+kubectl apply -f rec.yaml -n application
+
+kubectl apply -f db.yaml -n application
